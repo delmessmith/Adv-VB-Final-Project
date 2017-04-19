f65fbadd05a9af42010fc17964a85470bd6ef4e7
HOMEPC\Del <delmessmith@hotmail.com>
2017-04-18 20:56:06 -0400

    Added class for Vehicles

---
+++ VB Final Project/Vehicles.vb
@@ -0,0 +1,22 @@
+﻿Public Class Vehicles
+    Dim mName As String
+    Dim mID As Integer
+
+    Public Property VehicleID As String
+        Get
+            VehicleID = mID
+        End Get
+        Set(value As String)
+            mID = value
+        End Set
+    End Property
+
+    Public Property Name As String
+        Get
+            Name = mName
+        End Get
+        Set(value As String)
+            mName = value
+        End Set
+    End Property
+End Class
